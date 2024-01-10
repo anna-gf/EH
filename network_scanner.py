868383e1958ae0fb7215c06b135e5e6b4a956eb0
Anna <anna.gasparin88@gmail.com>
2024-01-10 18:06:22 +0100

    ARP request directed to broadcast MAC

--- network_scanner.py
+++ network_scanner.py
@@ -8,9 +8,11 @@ import scapy.all as scapy
 """
 
 def scan(ip):
-    # Create packets by:    Use ARP to ask who has the target IP
-    #                       Set destination MAC to broadcast MAC
+    # Use ARP to ask who has the target IP --> create ARP request
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    # Set destination MAC to broadcast MAC --> destination is set in the ethernet part of the packet
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast= broadcast/arp_request
+    print(arp_request_broadcast.summary())
 
 scan("192.168.1.1/24")
