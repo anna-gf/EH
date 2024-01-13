8c880b870f54917eeeb537a495ea9f5217ec1693
Anna <anna.gasparin88@gmail.com>
2024-01-13 10:40:00 +0100

    Get only IP and MAC from the response

--- network_scanner.py
+++ network_scanner.py
@@ -7,16 +7,20 @@ import scapy.all as scapy
  MAC address is discovered by ARP that links IPs with MACs. Sends a broadcast message, ARP request, asking who has the X IP.
 """
 
-def scan(ip):
+def scan(ip, interface):
     # Use ARP to ask who has the target IP --> create ARP request
     arp_request = scapy.ARP(pdst=ip)
     # Set destination MAC to broadcast MAC --> destination is set in the ethernet part of the packet
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast= broadcast/arp_request
     # srp allows to send packets with a custom ehternet part
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered_list= scapy.srp(arp_request_broadcast,  iface=interface, timeout=1)[0]
+    print(answered_list.summary())
 
-scan("192.168.1.1/24")
+    for element in answered_list:
+        print(element[1].show)
+
+
+scan("192.168.1.1/24", "eth1")
 
 # Send packet and receive response
